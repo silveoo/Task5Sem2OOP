82fcee10933dc878a2115e7d26331b62e10cd48e
thevo <thevo@DESKTOP-04NMLJR>
2022-05-02 14:21:38 +0300

    Добавьте файлы проекта.

---
+++ Task5Sem2OOP/Task5Sem2OOP.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void sort(int*);
+
+int main()
+{
+    int* arr = new int[12];
+
+    for (int i = 0; i <= 11; i++)
+    {
+        arr[i] = (i+1);
+    }
+
+    cout << "[ ";
+
+    for (int i = 0; i <= 11; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+
+    cout << ']' << endl;
+    sort(arr);
+    cout << "[ ";
+
+    for (int i = 0; i <= 11; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+
+    cout << ']' << endl;
+
+    return 0;
+}
+
+void sort(int* arr)
+{
+    for (int i = 0; i < 11;)
+    {
+        int srt = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = srt;
+        i += 2;
+    }
+}
